0e3767077565bbe2161a40c38c81650a2966a086
wasabina67 <wasabina67@gmail.com>
2026-03-28 11:59:29 +0900

    Update docs/voice/data.js

--- docs/voice/data.js
+++ docs/voice/data.js
@@ -1,12 +1,12 @@
 const salesData = [
   {
     image: "images/1.jpg",
-    customer: "東京都 F様",
-    name: "トヨタ アクア",
-    year: "2019年式",
-    model: "S グレード",
-    mileage: "35,000 km",
-    deliveryDate: "2025年1月",
-    comment: "免許取りたてのお嬢様へのプレゼント。コンパクトで運転しやすく、親御さんも安心ですと喜ばれました。"
+    customer: "北海道 O様",
+    name: "スバル フォレスター",
+    year: "2012年式",
+    model: "2.0i-S アイサイト",
+    mileage: "46,000 km",
+    deliveryDate: "2026年3月",
+    comment: "現在乗っている車が古くなり買い替えを検討したいとご相談。兼ねてからSUVに乗りたいとのことでした。走行距離も短く、関東からの仕入れの為サビも無く、納車前には下回りの防錆施工も行いバッチリです。"
   },
 ];
